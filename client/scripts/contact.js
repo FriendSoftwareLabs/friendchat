10442bb2fb403723dc33a2ae9bc9035d7e2a7664
Espen Olsen <eo@friendup.cloud>
2022-01-26 11:51:41 +0000

    asd

--- client/scripts/contact.js
+++ client/scripts/contact.js
@@ -2410,7 +2410,7 @@ library.contact = library.contact || {};
 	
 	ns.PresenceRoom.prototype.handleChat = async function( event ) {
 		const self = this;
-		console.log( 'handleChat', event );
+		//console.log( 'handleChat', event );
 		const chat = {
 			type : 'chat',
 			data : event,
@@ -2937,7 +2937,7 @@ library.contact = library.contact || {};
 	
 	ns.PresenceRoom.prototype.sendChatEvent = function( e ) {
 		const self = this;
-		console.log( 'sendchatEvent', e );
+		//console.log( 'sendchatEvent', e );
 		const chat = {
 			type : 'chat',
 			data : e,
