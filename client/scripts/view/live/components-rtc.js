5b05f59bb1f885cfd1826e2d5be56fc496d55d72
Espen Olsen <eo@friendup.cloud>
2022-09-05 06:21:09 +0000

    asd

--- client/scripts/view/live/components-rtc.js
+++ client/scripts/view/live/components-rtc.js
@@ -801,7 +801,7 @@ library.rtc = library.rtc || {};
 		self.channels = {};
 		
 		// rtc specific logging ( automatic host / client prefix )
-		self.spam = true;
+		self.spam = false;
 		
 		self.init();
 	}
@@ -2428,7 +2428,7 @@ library.rtc = library.rtc || {};
 		
 		library.component.EventEmitter.call( self );
 		
-		self.spam = true;
+		self.spam = false;
 		
 		self.init();
 	}
