e2132b62037cd8f496375a89e1b2ad61b2fd5112
Espen Olsen <eo@friendup.cloud>
2022-09-13 12:32:10 +0000

    asd

--- client/scripts/view/live/components-rtc.js
+++ client/scripts/view/live/components-rtc.js
@@ -3339,7 +3339,7 @@ library.rtc = library.rtc || {};
 				return
 			}
 			
-			setTimeout( doItLater, 1000 )
+			setTimeout( doItLater, 1500 )
 			function doItLater() {
 				self.log( 'doing it now' )
 				Promise.all( vTracks.map( constrain ))
