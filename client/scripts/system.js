627fa37b6e8b67d7c82ffbd31e31eb2b7d8da3c7
Espen Olsen <eo@friendup.cloud>
2022-09-22 09:18:31 +0000

    asd

--- client/scripts/system.js
+++ client/scripts/system.js
@@ -1267,7 +1267,7 @@ library.rtc = library.rtc || {};
 		
 		const conf = {
 			type       : 'account',
-			title      : 'Account',
+			title      : 'Chat',
 			windowConf : winConf,
 			onsave     : saveHandler,
 			onclose    : closeHandler,
